cd3124cd47e1466e9dade09c77647b137dfb6121
Denchi <denchisoft@gmail.com>
2018-05-17 23:33:30 +0200

    Initial commit.

---
+++ CubismAR/README.md
@@ -0,0 +1,157 @@
+﻿# Live2D Cubism SDK AR.js Samples
+
+Live2D Cubism SDK For JavaScript と AR.js を組み合わせたサンプルです。
+
+アプリのダウンロードやインストールを行わず、ブラウザだけで AR コンテンツを提供することが可能です。
+
+名刺やポストカード、パンフレット、チラシなど、配布物や掲示物で効果的なプロモーションを行うことができます。
+
+下記のサンプルページを開き、PC やスマートフォンのカメラでマーカーを写せば、 Live2D のモデルが表示されます。
+
+マーカーにロゴ画像を使う例と QR コードを使う例の2種類を用意しています。
+
+### ロゴ画像をマーカーにしたサンプル
+|[サンプルページのリンク](https://live2d.github.io/CubismARjsSample/)|マーカー|
+|---|---|
+|<img src="https://live2d.github.io/CubismARjsSample/assets/logo_qr.png" width="300px">|<img src="https://live2d.github.io/CubismARjsSample/assets/logo_marker.png" width="300px">|
+
+### QR コードをマーカーにしたサンプル
+|[サンプルページのリンク兼マーカー](https://live2d.github.io/CubismARjsSample/qr_marker.html)|
+|---|
+|<img src="https://live2d.github.io/CubismARjsSample/assets/qr_marker.png" width="300px">|
+
+
+## インストールするには？
+
+1. [トップページ](https://github.com/Live2D/CubismARjsSample) の右上にある "Clone or download" ボタンを押します
+
+2. ポップアップが表示されるので、"Download ZIP" ボタンを押します
+
+3. ダウンロードした zip ファイルを解凍します
+
+4. 解凍した CubismARjsSample フォルダをウェブサーバーにアップロードします
+
+5. アップロード先の URL にアクセスし、カメラでマーカーを写せば、モデルが表示されます。
+
+## サーバーを用意せずにPCだけで試すには？（要ウェブカメラ）
+
+1. Google Chrome に [Web Server for Chrome](https://chrome.google.com/webstore/detail/web-server-for-chrome/ofhbbkphhbklhfoeikjpcbhemlocgigb?hl=ja) アドオンを追加します
+
+2. Web Server for Chrome を実行し、CHOOSE FOLDER ボタンで CubismARjsSample のあるフォルダを選択します
+
+3. ブラウザから [http://127.0.0.1:8887/CubismARjsSample/](http://127.0.0.1:8887/CubismARjsSample/) にアクセスします
+
+4. カメラでマーカーを映すとモデルが表示されます
+
+## オリジナルのモデルを使うには？
+
+1. assets フォルダの moc3 や json、テクスチャを差し替えます
+
+2. js フォルダの pixiAframeAR.js をテキスト形式で開き、各ファイルへのパスを差し替えたものに変更します
+
+3. オリジナルのモデルが表示されるようになります
+
+## オリジナルのマーカーを使うには？
+
+1. [AR.js Marker Training](https://jeromeetienne.github.io/AR.js/three.js/examples/marker-training/examples/generator.html) を開きます
+
+2. UPLOAD ボタンを押し、マーカーにしたい画像を選択します
+
+3. DOWNLOAD ボタンを押し、patt ファイルを assets フォルダに保存します
+
+4. html ファイルの a-marker タグにある url を patt ファイルのパスに変更します
+
+5. 画面中央に表示されている黒枠に囲まれた画像がマーカーとして使えるようになります
+
+## オリジナルのQRコードを使うには？
+
+QR コードの作成方法に指定はありません。
+
+ウェブ上にQRコード生成サービスが多数提供されていますので、お好きなものをお使いください。
+
+**QRコードを [AR.js Marker Training](https://jeromeetienne.github.io/AR.js/three.js/examples/marker-training/examples/generator.html) でマーカー画像に指定することも可能です。**
+
+## 機能
+
+- マーカーに対して並行、垂直にモデルを表示
+
+- モデルの複数体表示
+
+- ランダムモーション再生
+
+- カメラ方向への視線追従
+
+- クリック、タップでのモーション切り替え
+
+## 注意事項
+
+### aframe-ar.js の差し替えについて
+AR.js の master ブランチで配布されているコードに下記の不具合修正を加えています。
+
+aframe-ar.js を他のバージョンに差し替える際はご注意ください。
+
+- [オリジナルのマーカーを使うための修正](https://github.com/wimvdc/AR.js/commit/950e82db6d0c3851647d429282c5ade52ee95891)
+
+- [画面の向きで縦横比が崩れる不具合の修正](https://github.com/jeromeetienne/AR.js/pull/212/files)
+
+
+### マーカー検出のフレームレートについて
+描画のフレームレートとは別に、 a-scene タグ内でマーカーを検出するフレームレートを指定しています。
+
+検出のフレームレートを上げるとカメラを移動した際にモデルの表示位置がスムーズに追従していきますが、カメラの静止時も細かく位置や傾きが変動するため、モデルがブレて見えることがあります。
+
+検出のフレームレートはコンテンツの仕様に合わせて調整してください。
+
+## TODO
+
+- 録画機能
+
+- 設定値の分離（jsonファイル？）
+
+## ライセンス
+Live2D Cubism Core は Live2D Proprietary Software License で提供しています。
+ - Live2D Proprietary Software License 
+[日本語](http://www.live2d.com/eula/live2d-proprietary-software-license-agreement_jp.html) 
+[English](http://www.live2d.com/eula/live2d-proprietary-software-license-agreement_en.html) 
+   - live2dcubismcore.min.js
+
+Live2D Cubism Components は Live2D Open Software License で提供しています。
+- Live2D Open Software License 
+[日本語](http://www.live2d.com/eula/live2d-open-software-license-agreement_jp.html) 
+[English](http://www.live2d.com/eula/live2d-open-software-license-agreement_en.html) 
+   - live2dcubismframework.js
+   - live2dcubismpixi.js
+
+Live2D のサンプルモデルは Free Material License で提供しています。
+- Free Material License 
+[日本語](http://www.live2d.com/eula/live2d-free-material-license-agreement_jp.html) 
+[English](http://www.live2d.com/eula/live2d-free-material-license-agreement_en.html) 
+   - assets/Koharu/*
+   - assets/Haruto/*
+   - assets/Hiyori/*
+
+本サンプルの Cubism Core と Cubism Components に含まれないコードは MIT License で提供しています。
+ - MIT License
+[日本語](https://ja.osdn.net/projects/opensource/wiki/licenses%2FMIT_license)
+[English](https://opensource.org/licenses/mit-license.php)
+   - pixiAframeAR.js
+   - index.html
+   - qr_marker.html
+
+直近会計年度の売上高が 1000 万円以上の事業者様がご利用になる場合は、SDKリリース(出版許諾)ライセンスに同意していただく必要がございます。 
+- [SDKリリース(出版許諾)ライセンス](http://www.live2d.com/ja/products/releaselicense) 
+
+*All business* users must obtain a Publication License. "Business" means an entity  with the annual gross revenue more than ten million (10,000,000) JPY for the most recent fiscal year.
+- [SDK Release (Publication) License](http://www.live2d.com/en/products/releaselicense) 
+
+A-Frame は MIT License で提供されています。
+[A-Frame license](https://github.com/aframevr/aframe/blob/master/LICENSE)
+
+AR.js は MIT License で提供されています。
+[AR.js license](https://github.com/jeromeetienne/AR.js/blob/master/LICENSE.txt)
+
+PixiJS は MIT License で提供されています。
+[PixiJS license](https://github.com/pixijs/pixi.js/blob/dev/LICENSE)
+
+QRコードは(株)デンソーウェーブの登録商標です。
+
---
+++ CubismAR/js/pixiAframeAR.js
@@ -0,0 +1,221 @@
+window.onload = function () {
+	var marker = document.querySelector('a-marker');
+	if(!marker){ marker = document.querySelector('a-marker-camera'); }
+	var camera = document.querySelector("a-entity[camera]");
+	if(!camera){ camera = document.querySelector("a-marker-camera"); }
+	camera = camera.components.camera.camera;
+
+	//画面の回転フラグ
+	var orientationchanged = false;
+	//マーカーに対しての直立フラグ
+	var stand_mode = false;
+
+	var models = [];
+	var app = new PIXI.Application(0, 0, { transparent: true });
+	loadAssets().then(addModel).then(addPlane);
+
+	function loadAssets() {
+		//モーションの設定
+		function setMotion(model, resources, x, y, resolve, reject){
+			if (model == null){ reject(); }
+
+			//基本モーション
+			var motions = [];
+			var animation = LIVE2DCUBISMFRAMEWORK.Animation;
+			var override = LIVE2DCUBISMFRAMEWORK.BuiltinAnimationBlenders.OVERRIDE;
+			motions.push(animation.fromMotion3Json(resources['motion2'].data));
+			model.motions = motions;
+			model.animator.addLayer("motion", override, 1);
+			//ランダムでモーション再生
+			var rand = Math.floor(Math.random() * model.motions.length);
+			model.animator.getLayer("motion").play(model.motions[rand]);
+
+			//クリックモーション
+			var data = resources['motion1'].data;
+			model.click_motion = animation.fromMotion3Json(data);
+
+			//視線追従モーション
+			data.CurveCount = data.TotalPointCount = data.TotalSegmentCount = 0;
+			data.Curves = [];
+			var gaze_motion = animation.fromMotion3Json(data);
+			model.animator.addLayer("gaze", override, 1);
+			model.animator.getLayer("gaze").play(gaze_motion);
+
+			//視線追従モーションのパラメータ値更新
+			model.gaze = new THREE.Vector3();
+			var ids = model.parameters.ids;
+			var angle_x = Math.max(ids.indexOf("ParamAngleX"), ids.indexOf("PARAM_ANGLE_X"));
+			var angle_y = Math.max(ids.indexOf("ParamAngleY"), ids.indexOf("PARAM_ANGLE_Y"));
+			var eye_x = Math.max(ids.indexOf("ParamEyeBallX"), ids.indexOf("PARAM_EYE_BALL_X"));
+			var eye_y = Math.max(ids.indexOf("ParamEyeBallY"), ids.indexOf("PARAM_EYE_BALL_Y"));
+			gaze_motion.evaluate = (time, weight, blend, target, stackFlags, groups) => {
+				if(stand_mode){ model.gaze.y *= 0.1; }
+				var values = target.parameters.values;
+				var max = target.parameters.maximumValues;
+				var min = target.parameters.minimumValues;
+				var angle_h = model.gaze.x > 0 ? max[angle_x] : -min[angle_x];
+				var angle_v = model.gaze.y > 0 ? max[angle_y] : -min[angle_y];
+				var eye_h = model.gaze.x > 0 ? max[eye_x] : -min[eye_x];
+				var eye_v = model.gaze.y > 0 ? max[eye_y] : -min[eye_y];
+				values[angle_x] = blend(values[angle_x], model.gaze.x * angle_h, 0, weight);
+				values[angle_y] = blend(values[angle_y], model.gaze.y * angle_v, 0, weight);
+				values[eye_x] = blend(values[eye_x], model.gaze.x * eye_h, 0, weight);
+				values[eye_y] = blend(values[eye_y], model.gaze.y * eye_v, 0, weight);
+			}
+
+			//キャンバス内のモデルの位置
+			model.pos_x = x;
+			model.pos_y = y;
+
+			models.push(model);
+			resolve();
+		}
+		//アセットの読み込み
+		var xhrType = { xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.JSON };
+		var p1 = new Promise(function (resolve, reject) {
+			var loader = new PIXI.loaders.Loader();
+			loader.add('model3', "CubismAR/assets/Hideri/hideri__2.model3.json", xhrType);
+			loader.add('motion1', "CubismAR/assets/Hideri/Hide_Aweiku.motion3.json", xhrType);
+			loader.add('motion2', "CubismAR/assets/Hideri/Hide_Suriipu.motion3.json", xhrType);
+			loader.load(function (loader, resources) {
+				var builder = new LIVE2DCUBISMPIXI.ModelBuilder();
+				builder.buildFromModel3Json(loader, resources['model3'], complate);
+				function complate(model){ setMotion(model, resources, 0.42, 0.38, resolve, reject); }
+			});
+		});
+
+		return Promise.all([p1]);
+	}
+	function addModel() {
+		//モデルの登録
+		var p = new Promise(function (resolve, reject) {
+			models.forEach(function(model){
+				app.stage.addChild(model);
+				app.stage.addChild(model.masks);
+			});
+			app.stage.renderable = false;
+			app.ticker.add(function (deltaTime) {
+				models.forEach(function(model){
+					model.update(deltaTime);
+					model.masks.update(app.renderer);
+				});
+			});
+			resolve();
+		});
+		return Promise.all([p]);
+	}
+	function addPlane() {
+		var plane = document.createElement('a-plane');
+		plane.setAttribute('plane', '');
+		plane.setAttribute('color', '#000');
+		plane.setAttribute('height', '5');
+		plane.setAttribute('width', '5');
+		//マーカーを基準にしたモデルの相対位置
+		plane.setAttribute('position', '0 0 0');
+		var stand = stand_mode ? '0 0 0' : '0 0 0';
+		plane.setAttribute('rotation', stand);
+		marker.appendChild(plane);
+
+		plane.object3D.front = new THREE.Object3D();
+		plane.object3D.front.position.set(0, 0, -1);
+		plane.object3D.add(plane.object3D.front);
+
+		var texture = new THREE.Texture(app.view);
+		texture.premultiplyAlpha = true;
+		var material = new THREE.MeshStandardMaterial({});
+		material.map = texture;
+		material.metalness = 0;
+		material.premultipliedAlpha = true;
+		material.transparent = true;
+		var mesh = null;
+
+		AFRAME.registerComponent('plane', {
+			init: function () {
+				mesh = this.el.getObject3D('mesh');
+				mesh.material = material;
+			},
+			update: function(){
+				var width = 2048;
+				var height = 2048;
+				app.view.width = width + "px";
+				app.view.height = height + "px";
+				app.renderer.resize(width, height);
+
+				models.forEach(function(model){
+					model.position = new PIXI.Point(width * model.pos_x, height * model.pos_y);
+					model.scale = new PIXI.Point(width * 0.9, width * 0.9);
+					model.masks.resize(app.view.width, app.view.height);
+				});
+
+				mesh.material.map.needsUpdate = true;
+			},
+			tick: function (time, timeDelta) {
+				if(marker.object3D.visible){
+					//画面が回転した直後（＝モデルの表示位置がずれている）でないなら描画する
+					if(!orientationchanged){ app.stage.renderable = true; }
+					mesh.material.map.needsUpdate = true;
+
+					var pos = plane.object3D.getWorldPosition();
+					var gaze = plane.object3D.front.getWorldPosition();
+					gaze.sub(pos);
+					models.forEach(function(model){ 
+						//視線追従モーションの更新
+						model.gaze = gaze;
+
+						//ランダムでモーション再生
+						var motion = model.animator.getLayer("motion");
+						if(motion && motion.currentTime >= motion.currentAnimation.duration){
+							var rand = Math.floor(Math.random() * model.motions.length);
+							motion.stop();
+							motion.play(model.motions[rand]);
+						}
+					});
+				}else{
+					//マーカーが外れたら描画を止める
+					app.stage.renderable = false;
+					//マーカーが外れたら画面の回転フラグを折る
+					//→マーカーの再検出時にモデルの表示位置が修正されるため
+					orientationchanged = false;
+				}
+			}
+		});
+	}
+
+	var click_event = function (e) {
+		//クリックモーションの再生
+		models.forEach(function(model){ 
+			var motion = model.animator.getLayer("motion");
+			if(motion && model.click_motion){
+				motion.stop();
+				motion.play(model.click_motion);
+			}
+		});
+	}
+	//PCとスマホの選択イベントの振り分け
+	if(window.ontouchstart === undefined){
+		window.onclick = click_event;
+	}else{
+		window.ontouchstart = click_event;
+	}
+	window.onorientationchange = function (e) {
+		if (e === void 0) { e = null; }
+		//画面が回転するとモデルの表示位置がずれるため描画を止める
+		app.stage.renderable = false;
+		//画面の回転フラグを立てる
+		orientationchanged = true;
+	}
+};
+/*
+//FPSの表示
+var script = document.createElement('script');
+script.onload=function(){
+	var stats = new Stats();
+	document.body.appendChild(stats.dom);
+	requestAnimationFrame(function loop(){
+		stats.update();
+		requestAnimationFrame(loop)
+	});
+};
+script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';
+document.head.appendChild(script);
+*/
